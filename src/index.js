52581ab1ce4e105da2def338b43145605443599c
Giulio Riondino <riondino.giulio@gmail.com>
2024-12-23 07:20:41 -0800

    Prepare index event listeners

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import './reset.css';
 import './styles.css';
+
+const homeButton = document.querySelector('button.home');
+const menuButton = document.querySelector('button.menu');
+const aboutButton = document.querySelector('button.about');
+
+
+homeButton.addEventListener('click', () => {
+    
+});
+
+menuButton.addEventListener('click', () => {
+    
+});
+
+aboutButton.addEventListener('click', () => {
+    
+});
